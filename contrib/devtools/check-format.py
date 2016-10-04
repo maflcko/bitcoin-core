19f7713fe0a28752f808ddd36f1fbc54b4e70a0c
MarcoFalke <falke.marco@gmail.com>
2016-10-04 15:23:57 +0200

    f

--- contrib/devtools/check-format.py
+++ contrib/devtools/check-format.py
@@ -18,6 +18,9 @@ CMD_CLANG_FORMAT_DIFF = 'git diff -U0 HEAD~ | {}/clang-format-diff.py -p1 -i -v'
 CMD_GIT_DIFF = ['git', 'diff', '--exit-code']
 
 def main():
+  if os.getenv("TRAVIS_PULL_REQUEST", "false") == "false":
+    print("No TRAVIS_PULL_REQUEST request")
+    exit(0)
   check_call(CMD_CLANG_FORMAT_DIFF, shell=True)
   check_call(CMD_GIT_DIFF)
 
