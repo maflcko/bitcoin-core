25b4939b042ab6c00aa0f0c5f106b89e672d7a82
MarcoFalke <falke.marco@gmail.com>
2020-01-16 15:51:07 -0500

    test

--- ci/test/00_setup_env_i686_centos.sh
+++ ci/test/00_setup_env_i686_centos.sh
@@ -10,5 +10,5 @@ export HOST=i686-pc-linux-gnu
 export DOCKER_NAME_TAG=centos:7
 export DOCKER_PACKAGES="gcc-c++ glibc-devel.x86_64 libstdc++-devel.x86_64 glibc-devel.i686 libstdc++-devel.i686 ccache libtool make git python3 python36-zmq which patch lbzip2 dash"
 export GOAL="install"
-export BITCOIN_CONFIG="--enable-zmq --with-gui=qt5 --enable-reduce-exports"
+export BITCOIN_CONFIG="--enable-zmq --with-gui=qt5 --enable-reduce-exports LDFLAGS=-static-libstdc++"
 export CONFIG_SHELL="/bin/dash"
