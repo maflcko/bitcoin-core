191da1382a90afc34d9315afbad0933e07a3671c
MarcoFalke <falke.marco@gmail.com>
2020-03-17 16:05:09 -0400

    sort

--- src/util/time.h
+++ src/util/time.h
@@ -6,9 +6,9 @@
 #ifndef BITCOIN_UTIL_TIME_H
 #define BITCOIN_UTIL_TIME_H
 
+#include <chrono>
 #include <stdint.h>
 #include <string>
-#include <chrono>
 
 void UninterruptibleSleep(const std::chrono::microseconds& n);
 
